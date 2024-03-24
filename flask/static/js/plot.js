69ea52d06bdec51e398d9a78dc035d4b9b790606
Utkarsh Pratiush <utkarshpratiush@MacBook-Pro-147.local>
2024-03-24 17:37:01 -0400

    update

---
+++ flask/static/js/plot.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#div1").load("/pl");
+  });
+
+});
+
+function showDiv1() {
+   document.getElementById('div_1').style.display = "block";
+}
+
+function showDiv2() {
+   document.getElementById('div_2').style.display = "block";
+}
+
+function showDiv3() {
+   document.getElementById('div_3').style.display = "block";
+}
+
+function showDiv4() {
+   document.getElementById('div_4').style.display = "block";
+}
+
+function showDiv5() {
+   document.getElementById('div_5').style.display = "block";
+}
+
+
+function showDiv6() {
+   document.getElementById('div_6').style.display = "block";
+}
+
+function showDiv7() {
+   document.getElementById('div_7').style.display = "block";
+}
+
+function showDiv8() {
+   document.getElementById('div_8').style.display = "block";
+}
+
